bec0c4cdff6f486ff1cba637a8291e12d0ca3a49
liverHawk <hawkyofkn5365@gmail.com>
2025-06-06 14:28:24 +0900

    icon grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,16 @@ import react from '@astrojs/react';
 import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      devtoolsJson()
+    ]
   }
 });
